dc769cdb1aa4461b75991d40b4453b100220958d
striker666dk <xxxstriker666xxx@gmail.com>
2016-02-14 12:23:19 +0100

    Added SplashScreen

---
+++ app/src/main/java/com/elvborn/notepad/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.elvborn.notepad;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int delay = 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, delay);
+    }
+}
